8328963d6624769f77cae058d928e50f37c00aef
vinaymaheshwari <vinaymaheshwari35@gmail.com>
2021-01-11 16:52:13 +0530

    Adding Header and shop links to the app

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import ShopPage from './pages/shoppage/shop.component'
 
 import Homepage from "./pages/homepage/homepage.component"
  
+import Header from "./components/header/header.component"
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact  path="/" component={Homepage} />
         <Route  path="/shop" component={ShopPage} />
