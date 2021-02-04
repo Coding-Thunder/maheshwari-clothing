41b43ac8016f1028dba921c13f4cf93b624b96b0
vinaymaheshwari <vinaymaheshwari35@gmail.com>
2021-02-04 15:45:43 +0530

    added quantity check on the cart items

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,13 @@
-import React from "react";
-import CustomButton from "../custom-button/custom-button.component";
-import "./cart-dropdown.styles.scss";
+import React from 'react';
+
+import CustomButton from '../custom-button/custom-button.component';
+
+import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => (
-  <div className="cart-dropdown">
-    <div className="cart-items">
-      <CustomButton>GO TO CHECKOUT</CustomButton>
-    </div>
+  <div className='cart-dropdown'>
+    <div className='cart-items' />
+    <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
 
--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,4 +1,5 @@
-import {UserActionTypes} from "./user.action.types";
+import { UserActionTypes } from './user.types';
+
 const INITIAL_STATE = {
   currentUser: null
 };
