0c1e2963699daf1a45b8fe96aa9af9318eab76d8
vinaymaheshwari <vinaymaheshwari35@gmail.com>
2021-02-06 17:39:32 +0530

    added selector files to save some renders to improve performance of the application

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,22 +1,21 @@
-import CartActionTypes from './cart.types';
-import { addItemToCart } from './cart.utils';
-
+import CartActionTypes from "./cart.types";
+import { addItemToCart } from "./cart.utils";
 const INITIAL_STATE = {
   hidden: true,
-  cartItems: []
+  cartItems: [],
 };
 
-const cartReducer = (state = INITIAL_STATE, action) => {
+const cartReducer = (state = INITIAL_STATE, action, quantity) => {
   switch (action.type) {
     case CartActionTypes.TOGGLE_CART_HIDDEN:
       return {
         ...state,
-        hidden: !state.hidden
+        hidden: !state.hidden,
       };
     case CartActionTypes.ADD_ITEM:
       return {
         ...state,
-        cartItems: addItemToCart(state.cartItems, action.payload)
+        cartItems: addItemToCart(state.cartItems, action.payload),
       };
     default:
       return state;
