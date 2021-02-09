44644d9c3952170d42fbb8be4c0662500ba3230d
vinaymaheshwari <vinaymaheshwari35@gmail.com>
2021-02-09 09:18:18 +0530

    added checkout page

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 
+import { createStructuredSelector } from "reselect";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
@@ -19,8 +20,8 @@ const CartIcon = ({ toggleCartHidden, itemCount }) => (
 const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
-const mapStateToProps = (state) => ({
-  itemCount: selectCartItemCount(state),
+const mapStateToProps = createStructuredSelector({
+  itemCount: selectCartItemCount,
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
